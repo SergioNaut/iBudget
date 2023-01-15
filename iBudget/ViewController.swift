24a5a13ebabebb4c487bcd7dc3b7cfbd3a3f17d1
Salem Kosemani <salemsky95@gmail.com>
2023-01-15 12:22:59 -0500

    Initial Commit

---
+++ iBudget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iBudget
+//
+//  Created by Salem Kosemani on 2023-01-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
