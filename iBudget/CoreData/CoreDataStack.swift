2da46cf1fcdee8a50d0150f5c4bf175b421770fd
Chrisbrien-icloud <chrisbrien.glaze@icloud.com>
2023-01-29 00:13:03 -0500

    created user onboarding and implemented storage using CoreData

---
+++ iBudget/CoreData/CoreDataStack.swift
@@ -0,0 +1,48 @@
+//
+//  CoreDataStack.swift
+//  iBudget
+//
+//  Created by Chris-Brien Glaze on 28/01/2023.
+//
+
+import Foundation
+
+import CoreData
+
+class CoreDataStack {
+    
+    private let modelName: String
+
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+
+    private lazy var storeContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: self.modelName)
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                print("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+
+    lazy var managedContext: NSManagedObjectContext = self.storeContainer.viewContext
+
+    func getCoreDataContext () -> NSManagedObjectContext? {
+        return  AppDelegate.sharedAppDelegate.coreDataStack.managedContext
+    }
+    
+    
+    func saveContext() {
+        guard managedContext.hasChanges else { return }
+        do {
+            try managedContext.save()
+        } catch let error as NSError {
+            print("Unresolved error \(error), \(error.userInfo)")
+        }
+    }
+    
+    
+    
+}
