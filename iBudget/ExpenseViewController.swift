13213c0f078ce52607e07c778306b40ab68792aa
Pratik Gurung <gurung.pratik04@gmail.com>
2023-01-31 03:22:19 -0500

    navigation controller setup for tabbar

---
+++ iBudget/ExpenseViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseViewController.swift
+//  iBudget
+//
+//  Created by Pratik Gurung on 2023-01-31.
+//
+
+import Foundation
+import UIKit
+
+class ExpenseViewController: UIViewController {
+    
+    
+}
